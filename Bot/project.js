69ac72319a27ab8a217ba51377eb81bcea0ee012
Gilbert <gilberto21t@gmail.com>
2025-05-15 17:56:32 +1000

    client log on

---
+++ Bot/project.js
@@ -0,0 +1,20 @@
+const SteamUser = require('steam-user');
+const SteamTotp = require('steam-totp');
+const config = require('./config.json');
+
+const client = new SteamUser();
+
+const logOnOptions = {
+  accountName: config.username,
+  password: config.password,
+  twoFactorCode: SteamTotp.generateAuthCode(config.sharedSecret)
+};
+
+client.logOn(logOnOptions);
+
+client.on('loggedOn', () => {
+  console.log('Logged into Steam');
+
+  client.setPersona(SteamUser.EPersonaState.Online);
+  client.gamesPlayed(440);
+});
